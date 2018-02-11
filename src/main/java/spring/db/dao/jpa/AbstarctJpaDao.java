c2ada43fed071ee7ba8dc5c38a63842b12848769
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-02-11 17:31:02 +0300

    Lab-Spring: started a new lab: 7 ORM in spring (using Hibernate)

---
+++ src/main/java/spring/db/dao/jpa/AbstarctJpaDao.java
@@ -0,0 +1,12 @@
+package spring.db.dao.jpa;
+
+import javax.persistence.EntityManagerFactory;
+
+public class AbstarctJpaDao {
+
+    protected EntityManagerFactory emf;
+
+    public void setEntityManagerFactory(final EntityManagerFactory emf) {
+
+    }
+}
--- src/main/java/spring/model/ioc/SimplePerson.java
+++ src/main/java/spring/model/ioc/SimplePerson.java
@@ -1,17 +1,32 @@
 package spring.model.ioc;
 
 import lombok.Value;
+import lombok.experimental.Wither;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.util.List;
 
 @Value
 @Component("person")
+@javax.persistence.Entity
 public class SimplePerson implements Person {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
     private String firstName;
     private String lastName;
+
     private boolean isProgrammer;
+    @Wither
     private boolean isBroke;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
     private Country country;
     private List<Contact> contacts;
 
