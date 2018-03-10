0197941cfc589b0335fa7f7308209aa25bace398
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-03-10 14:16:34 +0300

    LAB: fun

--- src/main/java/spring/fun/ThreadMaster.java
+++ src/main/java/spring/fun/ThreadMaster.java
@@ -1,7 +1,13 @@
 package spring.fun;
 
 import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadMaster implements Runnable {
@@ -19,33 +25,48 @@ public class ThreadMaster implements Runnable {
     }
 
     public static void main(String[] args) throws Exception {
-        Thread thread = new Thread(() -> {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Integer> future = executorService.submit(() -> {
+            int result =  0;
             for (int i = 0; i < 5; i++) {
-                System.out.println("incremented first THREAD" + i);
-                try {
-                    TimeUnit.SECONDS.sleep(3);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
+                result+=i;
             }
+            return result;
         });
 
+        int x = future.get();
 
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 6; i++) {
-                System.out.println("incremented second THREAD" + i);
-                try {
-                    TimeUnit.SECONDS.sleep(4);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+        if (false) {
+            Thread thread = new Thread(() -> {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("incremented first THREAD" + i);
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
                 }
-            }
-        });
+            });
 
-        thread.start();
-        thread2.start();
 
-        thread2.join();
+            Thread thread2 = new Thread(() -> {
+                for (int i = 0; i < 6; i++) {
+                    System.out.println("incremented second THREAD" + i);
+                    try {
+                        TimeUnit.SECONDS.sleep(4);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
+            thread2.start();
+
+            thread2.join();
+        }
     }
 }
