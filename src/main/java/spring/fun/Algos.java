e7f287b1b726049cabe87d34ab67513f7d3b4adb
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-03-10 21:14:17 +0300

    LAB: fun

--- src/main/java/spring/fun/Algos.java
+++ src/main/java/spring/fun/Algos.java
@@ -4,7 +4,8 @@ public class Algos {
 
     public static void main(String[] args) {
         int [] arr = {55555,6,2,1,100,150,99,0,3,7,8700};
-        quickSort(arr);
+        //mergeSort(arr, 0, arr.length - 1);
+        insertionSort(arr);
         print(arr);
         binarySearch(arr,992);
     }
@@ -48,7 +49,7 @@ public class Algos {
             int var = array[i];
             int j = i - 1;
             while (j>=0 && var < array[j]) {
-                array[j+1] = array[j];
+                array[j+ 1] = array[j];
                 j--;
             }
             array[j+1] = var;
@@ -57,25 +58,65 @@ public class Algos {
 
     private static void selectionSort(int []array) {
         for (int i = 0; i < array.length; i++) {
-            int var = array[i];
             int index = i;
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < var) {
-                    var = array[j];
+            int var = array[i];
+            for (int j = i; j < array.length; j++) {
+                if (var < array[j]) {
                     index = j;
+                    var = array[j];
                 }
             }
-            if (index != i) {
-                int temp = array[i];
-                array[i] = array[index];
-                array[index] = temp;
-            }
-
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
         }
     }
 
-    private static void mergeSort(int []array) {
+    private static void mergeSort(int []array, int l, int r) {
+        if (l >= r)
+            return;
+        int middle = (l + r) / 2;
+        mergeSort(array, l, middle);
+        mergeSort(array, middle+1, r);
+        merge(array, l, middle, r);
 
+    }
+
+    private static void merge(int[] array, int l, int middle, int r) {
+        int n1 = middle - l + 1;
+        int n2 = r - middle;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        System.arraycopy(array, l, L, 0, n1);
+
+        for (int j = 0; j<n2; j++) {
+            R[j] = array[j + middle + 1];
+        }
+
+        int i =0, j = 0;
+        int k = l;
+        while(i<n1 && j<n2) {
+            if (L[i]<= R[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            array[k] = L[i];
+            k++;
+            i++;
+        }
+        while (i < n2) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
     private static void quickSort(int []array) {
