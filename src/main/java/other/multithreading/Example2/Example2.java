f6ddf3fdc5280446e0c2902baa285ae5ed21b1d8
Vlad Bagdey <Vladislav_Bagdei@epam.com>
2018-04-09 00:55:21 +0300

    upd

--- src/main/java/other/multithreading/Example2/Example2.java
+++ src/main/java/other/multithreading/Example2/Example2.java
@@ -9,24 +9,48 @@ public class Example2 {
     private static class SingleThreadExecutor {
 
 
-        private Queue<Runnable> tasks = new LinkedList<>();
+        private final Queue<Runnable> tasks = new LinkedList<>();
+        private final Object lock = new Object();
 
         public SingleThreadExecutor() {
             new Thread(this::process).start();
         }
 
-        private void process() {
-            while (true) {
-                while (!tasks.isEmpty()) {
-                    tasks.remove()
-                         .run();
+        private void process()  {
+            synchronized (tasks) {
+                if (tasks.isEmpty()) {
+                    try {
+                        System.out.println("Tasks list is empty!");
+                        tasks.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+                System.out.println("Running the task!");
+                tasks.remove().run();
             }
         }
 
         public void execute(Runnable task) {
             tasks.add(task);
-            tasks.notify();
+            synchronized (tasks) {
+                System.out.println("We got a new Task to complete!");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                tasks.notify();
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        SingleThreadExecutor executor = new SingleThreadExecutor();
+        System.out.println("Setting the execute!");
+        executor.execute(() -> {
+            System.out.println("Hello!");
+        });
+
     }
 }
